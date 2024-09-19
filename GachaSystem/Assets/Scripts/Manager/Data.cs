2a2ea999443ce513a5cb731fed763e66555e94bc
LeeHyeonCheol <60866286+Cocoa303@users.noreply.github.com>
2024-09-20 00:14:34 +0900

    DataManager 기초 설계
    - 저장, 불러오기 기능 구현
    - 기존 CSV 파일로 만들어둔 데이터 파일을 불러와 아이템의 타입에 따라 분류
    - 장비별 소환, UI 출력 등에서 빠른 접근을 위하여 Dictionary 사용
    - 한번 소유했던 아이템인지 검증하기 위하여 HashSet 사용
     -> 리스트보다 Contain 연산량이 적고, 중복 삽입이 안 되기 때문에 채용
    - 데이터 매니저가 아이템에 대한 정보를 모두 들고 있도록 제작
     -> 메모리 사용량을 조금 높이고 연산속도를 줄이기 위하여 채택
     -> 뽑기 / UI 출력 등에서 최적화를 목적으로 해당 방식으로 설계

---
+++ GachaSystem/Assets/Scripts/Manager/Data.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+using ItemType = global::Data.GachaRandomBag.GachaRewardItemType;
+
+namespace Manager
+{
+    public class Data : Util.Inherited.Singleton<Data>
+    {
+        private Dictionary<int, Item> items;         //== 모든 아이템의 정보
+        private Dictionary<ItemType, HashSet<int>> acquired;    //== 각 분류별 아이템의 소유 여부
+        private Dictionary<ItemType, HashSet<int>> isLevelUp;   //== 각 분류별 아이템의 레벨업 여부
+
+        private Dictionary<int, global::Data.GachaRandomBag> gachaRandomBags;
+
+        private const string itemPrefsKey = "Item Data Prefs Key";
+        private const char itemUnit = '|';
+        private const char dataUnit = ',';
+
+        public void Start()
+        {
+            Init();
+        }
+        public void OnApplicationQuit()
+        {
+            Save();
+        }
+
+        private void Init()
+        {
+            #region 데이터 기본 초기화
+
+            acquired = new Dictionary<ItemType, HashSet<int>>();
+            isLevelUp = new Dictionary<ItemType, HashSet<int>>();
+
+            var values = System.Enum.GetValues(typeof(ItemType));
+            foreach (var value in values)
+            {
+                ItemType type = (ItemType)value;
+
+                acquired.Add(type, new HashSet<int>());
+                isLevelUp.Add(type, new HashSet<int>());
+            }
+            #endregion
+
+            #region 데이터를 받아와 세팅 [ NOTE : 현재 클라이언트 내부 처리중 ]
+            var itemDic = new Dictionary<int, global::Data.ItemList>();
+            var itemTypeDic = new Dictionary<int, ItemType>();
+            items = new Dictionary<int, Item>();
+            gachaRandomBags = new Dictionary<int, global::Data.GachaRandomBag>();
+
+            string pathFront = "Data/";
+            string itemDataPath = pathFront + "ItemList";
+            string gachaDataPath = pathFront + "GachaRandomBag";
+
+            var findItemData = Resources.LoadAll<global::Data.ItemList>(itemDataPath);
+            for (int i = 0; i < findItemData.Length; i++)
+            {
+                if (itemDic.ContainsKey(findItemData[i].itemID))
+                {
+                    Debug.LogError($"[ {findItemData[i].itemID} ] ID 값을 가진 아이템 데이터가 이미 존재합니다.\n" +
+                        $"ItemList CSV를 확인해주세요.");
+                    continue;
+                }
+                itemDic.Add(findItemData[i].itemID, findItemData[i]);
+            }
+
+            var findGachaData = Resources.LoadAll<global::Data.GachaRandomBag>(gachaDataPath);
+            for (int i = 0; i < findGachaData.Length; i++)
+            {
+                if (gachaRandomBags.ContainsKey(findGachaData[i].dropID))
+                {
+                    Debug.LogError($"[ {findGachaData[i].dropID} ] ID 값을 가진 아이템 데이터가 이미 존재합니다.\n" +
+                        $"GachaRandomBag CSV를 확인해주세요.");
+                    continue;
+                }
+                gachaRandomBags.Add(findGachaData[i].dropID, findGachaData[i]);
+                if (itemTypeDic.ContainsKey(findGachaData[i].gachaRewardID)) continue;
+                itemTypeDic.Add(findGachaData[i].gachaRewardID, findGachaData[i].gachaRewardItemType);
+            }
+
+            #region 저장된 데이터 호출
+            string saveItemData = PlayerPrefs.GetString(itemPrefsKey, string.Empty);
+            var saveItemDic = new Dictionary<int, (int hasCount, int level)>();
+            if (saveItemData != string.Empty)
+            {
+                string[] packets = saveItemData.Split(itemUnit);
+
+                if(packets.Length > 1)
+                {
+                    for(int i = 0; i < packets.Length; i++)
+                    {
+                        //== 0 : id
+                        //== 1 : has count
+                        //== 2 : level
+                        string[] packet = packets[i].Split(dataUnit);
+
+                        int id = int.Parse(packet[0]);
+                        int hasCount = int.Parse(packet[1]);
+                        int level = int.Parse(packet[2]);
+
+                        if (!saveItemDic.ContainsKey(id))
+                        {
+                            saveItemDic.Add(id, (hasCount, level));
+                        }
+                        if(itemTypeDic.ContainsKey(id))
+                        {
+                            acquired[itemTypeDic[id]].Add(id);
+                        }
+                    }
+                }
+            }
+            #endregion
+
+            #region Item 데이터 세팅
+            foreach (var id in itemDic.Keys)
+            {
+                if (!items.ContainsKey(id))
+                {
+                    items.Add(id, new Item(
+                         itemDic[id],
+                        (saveItemDic.ContainsKey(id)) ? saveItemDic[id].hasCount : 0,
+                        (saveItemDic.ContainsKey(id)) ? saveItemDic[id].level : 0,
+                        itemTypeDic[id]
+                        ));
+                }
+            }
+            #endregion
+            #endregion
+
+        }
+
+        private void Save()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            foreach (var id in items.Keys)
+            {
+                Item item = items[id];
+
+                if (acquired[item.Type].Contains(id))
+                {
+                    //== 0 : id
+                    //== 1 : has count
+                    //== 2 : level
+
+                    builder.Append(id);
+                    builder.Append(dataUnit);
+                    builder.Append(item.HasCount);
+                    builder.Append(dataUnit);
+                    builder.Append(item.Level);
+                    builder.Append(itemUnit);
+                }
+            }
+
+            if(builder.Length > 0)
+            {
+                //== 마지막 itemUnit 삭제
+                builder = builder.Remove(builder.Length - 1, 1);
+            }
+
+            PlayerPrefs.SetString(itemPrefsKey, builder.ToString());
+        }
+    }
+}
+
