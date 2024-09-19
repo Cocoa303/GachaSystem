dca9c581564ebc5e27fd179d92e02291ad9b503f
LeeHyeonCheol <60866286+Cocoa303@users.noreply.github.com>
2024-09-19 23:58:53 +0900

    Item class 제작
    ItemList Data를 들고있으며, 보유 개수, 레벨, 타입등을 포함한 기본틀을 제작

---
+++ GachaSystem/Assets/Scripts/InGame/Item.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using ItemType = global::Data.GachaRandomBag.GachaRewardItemType;
+
+[System.Serializable]
+public class Item
+{
+    [SerializeField] protected Data.ItemList data;
+    [SerializeField] protected int hasCount;
+    [SerializeField] protected int level;
+    [SerializeField] protected ItemType type;
+
+    public Data.ItemList Data { get => data; set=> data = value; }
+    public int HasCount { get => hasCount; set => hasCount = value; }
+    public int Level { get => level; set => level = value; }
+    public ItemType Type { get => type; set=> type = value; }
+
+    public Item() { }
+    public Item(Data.ItemList data,int hasCount, int level, ItemType type)
+    {
+        this.data = data;
+        this.hasCount = hasCount;
+        this.level = level;
+        this.type = type;
+    }
+}
