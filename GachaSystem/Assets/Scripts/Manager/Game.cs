aae39927cf50e5a1714c33e14185476587230250
LeeHyeonCheol <60866286+Cocoa303@users.noreply.github.com>
2024-09-21 23:45:11 +0900

    ADD : Game Manager
    - 게임 내부 로직을 전반적으로 담당할 예정인 Game Manager를 추가하였습니다.
     - 머니 획득 관련된 로직을 추가하였습니다.
     - 해당 로직에서 Data Manager에 수시로 접근하는 것은 낭비라고 판단하여, 캐싱하여 사용하고 있습니다.
     - Save Money의 값 변화에 따라 이벤트가 발생하도록 Data Manager에 이벤트를 추가했습니다.
     - 해당 이벤트는 최대 머니량에 도달했을 때, 생산을 멈추고 그 외의 경우 생산을 재기하도록 설정되어 있습니다.

---
+++ GachaSystem/Assets/Scripts/Manager/Game.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Manager
+{
+    public class Game : Util.Inherited.Singleton<Game>
+    {
+        [SerializeField, ReadOnly] float refillMoneyCurrentTime;
+        [SerializeField, ReadOnly] float remainedMoney;
+        [SerializeField, ReadOnly] bool isRefiilAble = true;
+
+        public delegate void OnChangeValue<T>(T value);
+        public OnChangeValue<float> onChangeRefillMoneyInterval;
+
+        private float refillMoneyInterval;
+        private float refillMoneyCount;
+        private float maxMoneyLimit;
+
+        private void Start()
+        {
+            refillMoneyInterval = Data.Instance.GlobalValue("n_RefillMoneyInterval").value;
+            refillMoneyCount = Data.Instance.GlobalValue("n_RefillMoneyCount").value;
+            maxMoneyLimit = Data.Instance.GlobalValue("n_MaxMoneyLimit").value;
+            remainedMoney = 0.0f;
+
+            Data.Instance.Goods.InsertCallback("Save Money", OnSaveMoneyCallback);
+
+            OnSaveMoneyCallback(Data.Instance.Goods.Get("Save Money"));
+        }
+
+        private void OnSaveMoneyCallback(long saveMoney)
+        {
+            if (saveMoney < maxMoneyLimit)
+            {
+                isRefiilAble = true;
+            }
+            else
+            {
+                isRefiilAble = false;
+            }
+        }
+
+        private void Update()
+        {
+            if (!isRefiilAble) return;
+
+            refillMoneyCurrentTime -= Time.deltaTime;
+            if(refillMoneyCurrentTime <= 0)
+            {
+                refillMoneyCurrentTime = refillMoneyInterval;
+
+                remainedMoney += refillMoneyCount;
+
+                if (1 <= remainedMoney)
+                {
+                    int saveCount = (int)remainedMoney;
+                    remainedMoney -= saveCount;
+
+                    Data.Instance.Goods.Add("Save Money", saveCount);
+                }
+            }
+
+            onChangeRefillMoneyInterval?.Invoke(refillMoneyCurrentTime);
+        }
+    }
+}
+
