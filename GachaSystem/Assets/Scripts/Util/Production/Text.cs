5ffdea49e1776c80e9aa5e81cfce7181b48266f2
LeeHyeonCheol <60866286+Cocoa303@users.noreply.github.com>
2024-09-21 23:53:34 +0900

    Add : Production Text
    - Sprite Animation 방식을 참고하여, Text Animation을 구현하였습니다.
     - 이는 메인 화면에서 조금 더 몰입감을 주기 위하여 추가하였습니다.

---
+++ GachaSystem/Assets/Scripts/Util/Production/Text.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Util.Production
+{
+    public class Text : UIBehaviour
+    {
+        [SerializeField] private UnityEngine.UI.Text ui;
+        [SerializeField] private List<string> strings;
+
+        [SerializeField] private float duration;
+        [SerializeField, ReadOnly] private float currentTime;
+        [SerializeField, ReadOnly] private int currentIndex;
+
+        protected override void Start()
+        {
+            base.Start();
+            currentIndex = 0;
+            currentTime = 0;
+
+            if(strings == null || strings.Count == 0)
+            {
+                //== Componet 삭제
+                Destroy(this);
+            }
+
+            ui.text = strings[currentIndex];
+        }
+
+        private void Update()
+        {
+            currentTime += Time.deltaTime;
+            if(duration <= currentTime)
+            {
+                currentTime %= duration;
+                currentIndex = (currentIndex + 1)% strings.Count;
+
+                ui.text = strings[currentIndex];
+            }
+        }
+    }
+}
+
