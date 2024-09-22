54cf01936c83b78d64364768f30c7e7f50e3b71b
LeeHyeonCheol <60866286+Cocoa303@users.noreply.github.com>
2024-09-22 23:35:53 +0900

    Add : Resource Manager
    - Resource를 종합적으로 관리할 Manager를 추가하였습니다.
     - Atlas를 Resource Manager에 포함하였습니다. Start에서 Load합니다.
     - 접근성을 위하여 Dictionary를 사용하였기 때문에 동일한 이름을 검증하는 코드가 삽입되었습니다.

---
+++ GachaSystem/Assets/Scripts/Manager/Resource.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+namespace Manager
+{
+    public class Resource : Util.Inherited.Singleton<Resource>
+    {
+        private Dictionary<string, SpriteAtlas> atlasDB;
+
+
+        private readonly string atlasPath = "Atlas/";
+
+        private void Start()
+        {
+            Load();
+        }
+
+        private void Load()
+        {
+            var loadAtlas = Resources.LoadAll<SpriteAtlas>(atlasPath);
+
+            atlasDB = new Dictionary<string, SpriteAtlas>(loadAtlas.Length);
+
+            foreach (var item in loadAtlas)
+            {
+                if (atlasDB.ContainsKey(item.name))
+                {
+                    Debug.LogError($"{item.name} Atlas가 이미 존재합니다.\n");
+                    continue;
+                }
+
+                atlasDB.Add(item.name, item);
+            }
+        }
+        private Sprite GetSprite(string atlas, string name)
+        {
+            if(atlasDB.ContainsKey(atlas))
+            {
+                return atlasDB[atlas].GetSprite(name);
+            }
+
+            return null;
+        }
+    }
+}
