1a310c7e1bab8b307ebb5e6a6a781718fd01b0bf
LeeHyeonCheol <60866286+Cocoa303@users.noreply.github.com>
2024-09-23 16:44:37 +0900

    Update : Control Tap
    - 열거형의 기본값은 요소로써 활용될 수 있기 때문에, 따로 예외 처리를 추가합니다.

--- GachaSystem/Assets/Scripts/Control/Tap.cs
+++ GachaSystem/Assets/Scripts/Control/Tap.cs
@@ -66,7 +66,7 @@ namespace Control.UI
                 }
             }
 
-            if (Comparer<T>.Default.Compare(firstOpen, default) != 0)
+            if (Comparer<T>.Default.Compare(firstOpen, default) != 0 || typeof(T).IsEnum)
             {
                 OnSelect(firstOpen);
             }
