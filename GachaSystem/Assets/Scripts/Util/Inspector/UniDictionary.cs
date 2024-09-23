f005b5079d1416292c0a4036b40f3d25e67e88f7
LeeHyeonCheol <60866286+Cocoa303@users.noreply.github.com>
2024-09-24 01:11:58 +0900

    Add : Inspector Collection
    - Inspector내에서 값을 확인하기 위하여 2종류의 자료형을 추가하였습니다.
     - UniPair : KeyValuePair에 기반하여 만들었으나, 기존의 Readonly 방식이 아닌 값이 변경될 수 있는 형태로 사용됩니다. Inspector에서 사용 가능하다는 것과 단 2개의 형식이 필요할 때 따로 자료형을 지정하지 않고 사용하기 위하여 추가하였습니다.
     - UniDictionary : Dictionary의 내용을 Inspector에서 편하게 확인하기 위하여 만들었습니다.
      - Build시 Dictionary와 성능 차이가 매우 적은 구조로 만들었으며, 크기나 데이터가 변경되는 형식은 모두 상속받아 처리했습니다.

---
+++ GachaSystem/Assets/Scripts/Util/Inspector/UniDictionary.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Util.Inspector
+{
+    //== NOTE : 빌드시에 기존 Dictionary와 성능차이가 크게 나지 않으며
+    //          개발중에 Inspector내에서 데이터를 확인하는것이 주 목적이다.
+    //==        Inspector내에 데이터를 수정한다 하여, 기존 Dictionary에 데이터가 변동되지는 않는다.
+    [System.Serializable]
+    public class UniDictionary<TKey, TValue> : Dictionary<TKey, TValue> where TKey : System.IComparable<TKey>
+    {
+#if UNITY_EDITOR
+        [SerializeField, ReadOnly] private List<UniPair<TKey, TValue>> show = new List<UniPair<TKey, TValue>>();
+
+        //== NOTE : 접근 속도 안정화를 위하여 추가
+        private Dictionary<TKey, int> showIndex = new Dictionary<TKey, int>();
+#endif
+
+        public UniDictionary() : base() { }
+
+        public UniDictionary(IDictionary<TKey, TValue> dictionary) : base(dictionary) { ConstructorSynchronization(); }
+        public UniDictionary(IEnumerable<KeyValuePair<TKey, TValue>> collection) : base(collection) { ConstructorSynchronization(); }
+        public UniDictionary(IEqualityComparer<TKey> comparer) : base(comparer) { ConstructorSynchronization(); }
+        public UniDictionary(int capacity) : base(capacity) { ConstructorSynchronization(capacity); }
+        public UniDictionary(IDictionary<TKey, TValue> dictionary, IEqualityComparer<TKey> comparer) : base(dictionary, comparer) { ConstructorSynchronization(); }
+        public UniDictionary(IEnumerable<KeyValuePair<TKey, TValue>> collection, IEqualityComparer<TKey> comparer) : base(collection, comparer) { ConstructorSynchronization(); }
+        public UniDictionary(int capacity, IEqualityComparer<TKey> comparer) : base(capacity, comparer) { ConstructorSynchronization(capacity); }
+
+        private void ConstructorSynchronization(int capacity = 0)
+        {
+#if UNITY_EDITOR
+            show = new List<UniPair<TKey, TValue>>(capacity);
+            showIndex = new Dictionary<TKey, int>(capacity);
+
+            foreach (var key in Keys)
+            {
+                show.Add(new UniPair<TKey, TValue>(key, this[key]));
+                showIndex.Add(key, show.Count - 1);
+            }
+#endif
+        }
+
+        public new TValue this[TKey key]
+        {
+            get => base[key];
+            set
+            {
+                base[key] = value;
+#if UNITY_EDITOR
+                if (showIndex.ContainsKey(key))
+                {
+                    show[showIndex[key]].value = value;
+                }
+#endif
+            }
+        }
+
+        public new void Add(TKey key, TValue value)
+        {
+            base.Add(key, value);
+
+#if UNITY_EDITOR
+            show.Add(new UniPair<TKey, TValue>(key, value));
+            showIndex.Add(key, show.Count - 1);
+#endif
+        }
+        public new bool TryAdd(TKey key, TValue value)
+        {
+            bool result = base.TryAdd(key, value);
+#if UNITY_EDITOR
+            if (result)
+            {
+                show.Add(new UniPair<TKey, TValue>(key, value));
+                showIndex.Add(key, show.Count - 1);
+            }
+#endif
+            return result;
+        }
+        public new bool Remove(TKey key)
+        {
+            bool result = base.Remove(key);
+
+#if UNITY_EDITOR
+            if (result)
+            {
+                ConstructorSynchronization(show.Capacity);
+            }
+#endif
+
+            return result;
+        }
+        public new bool Remove(TKey key, out TValue value)
+        {
+            bool result = base.Remove(key, out value);
+
+#if UNITY_EDITOR
+            if (result)
+            {
+                ConstructorSynchronization(show.Capacity);
+            }
+#endif
+
+            return result;
+        }
+        public new void Clear()
+        {
+            base.Clear();
+
+#if UNITY_EDITOR
+            show.Clear();
+#endif
+        }
+    }
+}
---
+++ GachaSystem/Assets/Scripts/Util/Inspector/UniPair.cs
@@ -0,0 +1,20 @@
+namespace Util.Inspector
+{
+    [System.Serializable]
+    public class UniPair<T1, T2>
+    {
+        public T1 key;
+        public T2 value;
+
+        public UniPair(T1 key, T2 value)
+        {
+            this.key = key;
+            this.value = value;
+        }
+        public UniPair(UniPair<T1, T2> other)
+        {
+            this.key = other.key;
+            this.value = other.value;
+        }
+    }
+}
